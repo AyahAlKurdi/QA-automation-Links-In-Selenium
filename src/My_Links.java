dc76fdccc1fc5c1137ce0e22820457233e0eaaed
AyahAlkurdi <alkurdiaya.aa@gmail.com>
2022-09-24 13:00:25 +0300

    new

--- src/My_Links.java
+++ src/My_Links.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class My_Links {
 
 	public static void main(String[] args) {
-		// testing
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"D:\\ChromeDriver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
 
 	}
 
